d57f88be47ab5be0946ab083cb0a0a8138ac08c6
Tyler Robertson <tyler.evan.robertson@gmail.com>
2017-05-15 03:22:01 -0400

    contnued notes

--- course-3/week-1-docs/examples.js
+++ course-3/week-1-docs/examples.js
@@ -1,2 +1,12 @@
 /* Example Javascript code */
 
+alert("Hello, world!");
+
+prompt("Please enter your name: ");
+
+document.write("This should be at the bottom of the page...");
+document.write("...until more stuff gets added to it");
+
+console.log("The normal user will never see this...");
+
+element.innerHTML = "This will overwrite the previous contents of 'element'.";
